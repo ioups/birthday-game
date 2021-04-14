3a630a099f09c5e4936b14ea58e06e0de0394bde
Pierre-Emmanuel Daigre <pe.daigre@gmail.com>
2021-04-14 18:18:35 +0200

    add if to the JS load

--- app/javascript/packs/bdtest.js
+++ app/javascript/packs/bdtest.js
@@ -1,6 +1,6 @@
 console.log('Hello from My bdtest')
 
-const form = document.querySelector("#BlaBla");
+// const form = document.querySelector("#BlaBla");
 
 /* form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -15,8 +15,8 @@ const form = document.querySelector("#BlaBla");
 const form2 = document.querySelector("#new_test_form");
 const result = document.querySelector(".results"); // CSS id selector
 
-
-form2.addEventListener("submit", (event) => {
+if (form2) {
+  form2.addEventListener("submit", (event) => {
   event.preventDefault();
   const fn_form = document.querySelector("#username").value
   const age_form = document.querySelector("#dateinput").value
@@ -26,4 +26,5 @@ form2.addEventListener("submit", (event) => {
   console.log(nd.toDateString());
   result.insertAdjacentHTML("beforeend", `<p> Your Name is <strong>${fn_form}</strong></p>` );
   result.insertAdjacentHTML("beforeend", `Your date of birth is ${age_form}` );
-}); 
+  });
+}
